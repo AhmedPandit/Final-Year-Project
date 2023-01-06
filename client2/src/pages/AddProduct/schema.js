bd29bccf7e449631642b84f7a7058c37f8788c3d
AhmedPandit <80785872+AhmedPandit@users.noreply.github.com>
2023-01-06 14:09:28 +0500

    Delete client2 directory

--- client2/src/pages/AddProduct/schema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-  export const addproductSchema = yup.object().shape({
-    productname: yup.string().required("required"),
-    productprice: yup.number().positive().integer().required("required"),
-    productquantity: yup.number().positive().integer().required("required"),
-    productcateogry: yup.string().required("required"),
-    productseller: yup.string().required("required"),
- 
-  });
